bebe72fcc0e7cac0448a65a16871489b30c76d61
minsu Lee <dlalstn5789@gmail.com>
2022-04-28 02:27:11 +0900

    add

--- artihmetic/arithmetic_operations/add_AB.java
+++ artihmetic/arithmetic_operations/add_AB.java
@@ -1,11 +1,15 @@
 package arithmetic_operations;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class add_AB {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int A, B;
-		A = scanner.nextInt();
-		B = scanner.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+
 		System.out.print(A+B);
 	}
 }
