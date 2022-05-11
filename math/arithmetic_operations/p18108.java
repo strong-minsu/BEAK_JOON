eca47d3376e42a7c372c2fe2864f41019acec14f
minsu Lee <dlalstn5789@gmail.com>
2022-05-11 14:07:11 +0900

    commit message

---
+++ math/arithmetic_operations/p18108.java
@@ -0,0 +1,10 @@
+package arithmetic_operations;
+import java.util.*;
+public class p18108 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int year_b = scanner.nextInt();
+		int year_s = year_b - 543;
+		System.out.print(year_s);
+	}
+}
