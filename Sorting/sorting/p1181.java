42e9c65fe9a3df99706699ad11ce10621a3b66ed
minsu Lee <dlalstn5789@gmail.com>
2022-05-14 02:37:28 +0900

    commit message

--- Sorting/sorting/p1181.java
+++ Sorting/sorting/p1181.java
@@ -2,14 +2,7 @@ package sorting;
 import java.io.*;
 import java.util.*;
 
-//time out 2... ha..tlq..kf
 public class p1181{
-	/*public static String[] removeRE(String[] arrary, int index) {
-		List<String> tempSB = new ArrayList<String>(Arrays.asList(arrary));
-		tempSB.remove(index);
-		return tempSB.toArray(new String[0]);
-	}*/
-	
 	public static void main(String[] args) throws IOException  {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
@@ -17,57 +10,22 @@ public class p1181{
 		String temp = new String(" ");
 
 		//문자 배열 저장
-		int c=0;
 		for(int i=0; i<N; i++){
 			String sb = new String(sc.next());
 			O[i] = sb;
 		}
 		
-		// 인덱스를 삭제할 필요 X
-		/*for(int i=0; i<N; i++){
-			for(int j=i+1; j<N; j++) {
-				if(O[i].equals(O[j])) {
-					O = removeRE(O, j); //겹치는 인덱스 삭제
-					N = N-1; // 배열 크기 줄이기!
-				}
-			}
-		}*/
+		Arrays.sort(O);
+		//인덱스 길이를 기준으로 정렬하는 메소드
+		Arrays.sort(O, Comparator.comparing(String::length));
 		
-		int len = O.length;
-		
-		for(int i=0; i<len; i++) {
-			for(int j=i+1; j<len; j++) {
-				int s1 = O[i].length();
-				int s2 = O[j].length();
-				
-				if(s1 > s2){
-					temp = O[j];
-					O[j] = O[i];
-					O[i] = temp;
-				}
-				
-				else if(s1 == s2) {
-					int compared = O[i].compareTo(O[j]);
-					if (compared > 0) {
-						temp = O[j];
-						O[j] = O[i];
-						O[i] = temp;
-					}
-				}
-			}
-		}
-		for(int i=0; i<len; i++){
-			if(i==0) {
-				System.out.println(O[i]);
+		for(int i = 0; i<O.length; i++) {
+			if(temp.equals(O[i])) {
+				continue;
 			}
 			else {
-				int compared = O[i].compareTo(O[i-1]);
-				if(compared == 0) {
-					i++;
-					System.out.println(O[i]);
-				}
-				else
-					System.out.println(O[i]);
+				System.out.println(O[i]);
+				temp = O[i];
 			}
 		}
 	}
