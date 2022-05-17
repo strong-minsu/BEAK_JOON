c8ecb18fe07961ba72fdb95810cc330781771a6d
minsu Lee <dlalstn5789@gmail.com>
2022-05-17 22:43:57 +0900

    commit message

---
+++ Math/math/p1476.java
@@ -0,0 +1,29 @@
+package math;
+import java.util.*;
+
+public class p1476 {
+	//(1 ¡Â E ¡Â 15, 1 ¡Â S ¡Â 28, 1 ¡Â M ¡Â 19)
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int E = sc.nextInt();
+		int S = sc.nextInt();
+		int M = sc.nextInt();
+		int e=1, s=1, m=1;
+		int year = 1;
+		while(true) {
+			if(e > 15)
+				e=1;
+			if(s>28)
+				s=1;
+			if(m>19)
+				m=1;
+			if(e == E && s == S && m == M)
+				break;
+			e++;
+			s++;
+			m++;
+			year++;
+		}
+		System.out.print(year);
+	}
+}
